db965aa340a1e3a50de34b8f9cd87dd45f438cba
hiteshwar <hiteshwarkaim@gmail.com>
2024-11-22 10:13:03 +0530

    send response HttpStatus

--- src/main/java/com/hitesh/todo/todo_manager/controllers/TodoController.java
+++ src/main/java/com/hitesh/todo/todo_manager/controllers/TodoController.java
@@ -5,6 +5,8 @@ import com.hitesh.todo.todo_manager.service.TodoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +22,9 @@ public class TodoController {
     private TodoService todoService;
 
     @PostMapping
-    public Todo createTodo(@RequestBody Todo todo) {
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
         logger.info("Received todo: {}", todo);
         Todo todo1 = todoService.createTodo(todo);
-        return todo1;
+        return new ResponseEntity<>(todo1, HttpStatus.CREATED);
     }
 }
