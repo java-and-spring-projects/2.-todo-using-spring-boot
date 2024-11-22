f9ae16c45515b57656d1b4d9073d14efd013560b
hiteshwar <hiteshwarkaim@gmail.com>
2024-11-22 10:10:37 +0530

    configure and user logger

--- src/main/java/com/hitesh/todo/todo_manager/controllers/TodoController.java
+++ src/main/java/com/hitesh/todo/todo_manager/controllers/TodoController.java
@@ -2,6 +2,8 @@ package com.hitesh.todo.todo_manager.controllers;
 
 import com.hitesh.todo.todo_manager.model.Todo;
 import com.hitesh.todo.todo_manager.service.TodoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
+
+    Logger logger= LoggerFactory.getLogger(TodoController.class);
+
     @Autowired
     private TodoService todoService;
 
     @PostMapping
     public Todo createTodo(@RequestBody Todo todo) {
+        logger.info("Received todo: {}", todo);
         Todo todo1 = todoService.createTodo(todo);
         return todo1;
     }
