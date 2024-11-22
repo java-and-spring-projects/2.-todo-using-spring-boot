6c1650f733a7c977b874738b68a87a6d69f77dbe
hiteshwar <hiteshwarkaim@gmail.com>
2024-11-22 12:25:20 +0530

    get file in response

--- src/main/java/com/hitesh/todo/todo_manager/controllers/FileController.java
+++ src/main/java/com/hitesh/todo/todo_manager/controllers/FileController.java
@@ -1,13 +1,16 @@
 package com.hitesh.todo.todo_manager.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -35,5 +38,18 @@ public class FileController {
         }
 
         return "Files uploaded successfully";
+    }
+
+    //serving image file in response
+    @GetMapping("/serve-image")
+    public void serveImage(HttpServletResponse response) {
+        try{
+            InputStream fileInputStream = new FileInputStream("images/hitesh.jpg");
+            response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+            StreamUtils.copy(fileInputStream, response.getOutputStream());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
